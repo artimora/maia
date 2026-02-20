f0507ea6c4e6371601654d5c0d1b59e2c54dd3e7
copperdevs <github@copperdevs.com>
2026-02-20 03:18:52 -0600

    handle return

--- src/Artimora.Maia/Functions/GenericFunctionHandler.cs
+++ src/Artimora.Maia/Functions/GenericFunctionHandler.cs
@@ -8,42 +8,59 @@ public class GenericFunctionHandler(HandlerMetaData.Side side) : IFunctionHandle
 
     private readonly Dictionary<string, Func<Dictionary<string, string>, Dictionary<string, string>>> functions = [];
 
+    private readonly Dictionary<Guid, Message> returnQueue = [];
+
     public void OnMessage(int client, Message message)
     {
-        if (message.id != "artimora.function_call")
-            return;
+        if (message.id == "artimora.function_call")
+        {
+            var functionName = message["artimora:function_name"];
+            var functionReturnId = message["artimora:function_return_id"];
 
-        var functionName = message["artimora:function_name"];
-        var functionReturnId = message["artimora:function_return_id"];
+            var func = functions[functionName];
 
-        var func = functions[functionName];
+            var results = func(message.GetValues());
+            results["artimora:function_name"] = functionName;
+            results["artimora:function_return_id"] = functionReturnId;
 
-        var results = func(message.GetValues());
-        results["artimora:function_name"] = functionName;
-        results["artimora:function_return_id"] = functionReturnId;
+            var toSend = new Message("artimora:function_results");
+            toSend.SetValues(results);
 
-        var toSend = new Message("artimora:function_results");
-        toSend.SetValues(results);
+            messageSender?.Invoke(new FunctionSenderData(side, toSend, client));
+        }
 
-        messageSender?.Invoke(new FunctionSenderData(side, toSend, client));
+        if (message.id == "artimora:function_results")
+        {
+            var returnId = Guid.Parse(message["artimora:function_return_id"]);
+            returnQueue[returnId] = message;
+        }
     }
 
     public void RegisterMessageSender(Action<FunctionSenderData> sender) => messageSender = sender;
 
-    public Task<Dictionary<string, string>> CallFunction(string functionName, Dictionary<string, string> args)
+    public async Task<Dictionary<string, string>> CallFunction(string functionName, Dictionary<string, string> args)
     {
         var targetClient = args.TryGetValue("artimora:target_client", out var arg)
             ? int.Parse(arg)
             : -1;
 
+        var id = Guid.NewGuid();
+
         args["artimora:function_name"] = functionName;
         args["artimora:function_return_id"] = Guid.NewGuid().ToString();
 
         var toSend = new Message("artimora:function_call");
-        
+
         messageSender?.Invoke(new FunctionSenderData(side, toSend, targetClient));
-        
-        // TODO: now we need to get the message result back here, then return the values
+
+        await Task.Run(async () =>
+        {
+            while (!returnQueue.ContainsKey(id))
+                await Task.Delay(100);
+        });
+
+        var results = returnQueue[id];
+        return results.GetValues();
     }
 
     public void RegisterFunction(string functionName, Func<Dictionary<string, string>, Dictionary<string, string>> func, bool forceSet = false)
